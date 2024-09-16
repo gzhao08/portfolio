010558944b8407598cf10bc60d6cbac508797644
gzhao08 <gordonzhao00@gmail.com>
2024-09-16 11:25:30 -0700

    add logo and photo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Assuming you have a CSS file for styling
+import logo from "./gz-full_logo.png";
+import profilePic from "./china-photo-cropped.JPG";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className='profilePic'>
+        <img src={profilePic} alt="Profile" className="circular-image"/>
+      </div>
+      <div className="image-container">
+          		<img src={logo} alt="Profile" className="logo"/>
+      </div>
+      <h1>Gordon Zhao</h1>
     </div>
   );
 }
